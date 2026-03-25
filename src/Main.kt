2cb612e982fba0f696b05f63d575d972123a270a
h <privetnekto21@gmail.com>
2026-03-25 11:18:44 +0300

    GS-1-2

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
 fun main() {
-    println("Здравствуйте Нургали Асланович")
-    println("ffff")
-    println("ffff")
-    println("ffff")
-    println("Здравствуйте Нургали Асланович")
+    val applePrice: Double = 5.0
+    val applesAmount: Int = 10
 
+
+    var totalPrice: Double = applePrice * applesAmount
+
+
+    totalPrice += totalPrice * 0.05
+
+
+    println("налог: $totalPrice")
 }
